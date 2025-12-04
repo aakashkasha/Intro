b9ffd2339fb6bd3c4759d2a953bc48de73cb17e5
aakashkasha <aakashkasha09@gmail.com>
2025-12-04 14:43:27 +1100

    learnt about indentifiers and print statements

---
+++ intro1.py
@@ -0,0 +1,21 @@
+# print("Welcome to the world of programming")
+# print(4)
+# print("Hello python \n")
+# print("hello",5)
+# print("welcome to ", end="*")
+# print("test")
+
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+# codingal = "codingal"
+# print(codingal)
+# name = input("enter your name")
+# print("\nHello", name, "\n welcome to codingal")
+# print(f'hello {name}welcome to codingal')
+import keyword
+print("Python keywords are...\n")
+print(keyword.kwlist)
+
+      
